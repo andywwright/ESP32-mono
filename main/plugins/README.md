679cd58073d11cb1bed2925a22654c1f3bb422bb
andy <andy@navy.gov>
2025-06-04 20:56:05 +0100

    Flatten: remove submodule metadata and restore plain folders

---
+++ main/plugins/README.md
@@ -0,0 +1,182 @@
+## Assorted small plugins
+
+### Probe relay(s)
+
+Adds setting `$678` \(toolsetter\) and `$679` \(secondary probe\) for which auxillary ports to use for controlling the probe selection relay(s). Set to `-1` when unused/disabled.  
+Probe selection is via the inbuilt `G65P5Q<n>` macro, `<n>` is the probe id: 0 - primary probe, 1 - toolsetter, 2 - secondary probe.
+The toolsetter can be selected automatically on "@G59.3" toolchanges.
+
+Dependencies:
+
+The selected driver/board must provide at least one free auxiliary digital output port capable of driving the relay coil, either directly or via a buffer.
+
+### Feed override
+
+Adds Marlin style [M220](https://marlinfw.org/docs/gcode/M220.html) command for setting feed overrides.
+
+```
+  M220 [B] [R] [S<percent>]
+
+  B - backup current values
+  R - restore values from backup
+  S<percent> - percentage of current feedrate.
+```
+> [!NOTE]
+> `M220RS<percentage>` can be used to override the rapids rate, if R is not specified the feed rate will be overridden. This deviates from the Marlin specification.
+
+Configuration:
+
+Add/uncomment `#define FEED_OVERRIDE_ENABLE 1` in _my_machine.h_.
+
+### Homing pulloff
+
+*** Experimental ***
+
+Adds per axis setting for homing pulloff distance. `$290` for X-axis pulloff, `$291` for Y-axis, ...
+
+Configuration:
+
+Add/uncomment `#define HOMING_PULLOFF_ENABLE 1` in _my_machine.h_.
+
+### ESP-AT
+
+*** Experimental ***
+
+Adds Telnet support via [ESP-AT](https://docs.espressif.com/projects/esp-at/en/latest/esp32/Get_Started/index.html) running on a supported ESP MCU.
+Allows senders to connect to the controller via WiFi.
+
+Dependencies:
+
+Driver and board support for a serial port \(UART\) that is not claimed by a different plugin.
+
+Configuration:
+
+Add/uncomment `#define ESP_AT_ENABLE 1` in _my_machine.h_.
+
+Adds many networking and WiFi settings for configuring mode \(Station, Access Point\), Telnet port, IP adress etc.
+
+> [!NOTE]
+> The ESP32 is a 3.3V device and pins are not 5V tolerant. If the controller serial port uses 5V signalling add a 5V to 3.3V level shifter, at least for the controller TX line.
+
+### RGB LED strips
+
+Adds one or two settings, `$536` and `$537`, for setting number of LEDs in NeoPixel/WS2812 LED strips.
+
+Dependencies:
+
+Driver and board support for LED strips.
+
+Configuration:
+
+Add/uncomment `#define RGB_LED_ENABLE 1` in _my_machine.h_.
+
+### RGB LED strip control
+
+Adds support for Marlin style [M150 command](https://marlinfw.org/docs/gcode/M150.html).
+
+```
+M150 [B<intensity>] [I<pixel>] [K] [P<intensity>] [R<intensity>] [S<strip>] [U<intensity>] [W<intensity>]
+
+    B<intensity> - blue component, 0 - 255.
+    I<pixel>     - LED index, 0 - 255. Available if number of LEDs in strip is > 1.
+    K            - keep unspecified values.
+    P<intensity> - brightness, 0 - 255.
+    S<strip>     - strip index, 0 or 1. Default is 0.
+    R<intensity> - red component, 0 - 255.
+    U<intensity> - red component, 0 - 255.
+```
+
+Dependencies:
+
+Driver and board support for LED strips.
+
+Configuration:
+
+Add/uncomment `#define RGB_LED_ENABLE` in _my_machine.h_ and set/change the define value to `2`.
+
+Dependencies:
+
+Driver and board support for LED strips.  
+> [!NOTE]
+> If B axis is enabled then the plugin will not be activated.
+
+### Bind events to aux output ports
+
+Configuration:
+
+Add/uncomment `#define EVENTOUT_ENABLE 1` in _my_machine.h_ .
+
+Depending on the number of free output ports up to four events can be selected.
+Settings `$750+<n>` is used to select the event \(trigger\) to bind to the port selected by setting `$760+<n>`
+where `<n>` is the event number, currently 0 - 3.
+
+Dependencies:
+
+The selected driver/board must provide at least one free auxiliary output port.
+
+Credits:
+
+Based on idea from [plugin](https://github.com/Sienci-Labs/grblhal-switchbank) by @Sienci-Labs.
+
+### PWM Servo
+
+*** Experimental ***
+
+Adds support for Marlin style [M280](https://marlinfw.org/docs/gcode/M280.html) command.
+
+```
+M280 [P<index>] [S<position>]
+
+    P<index>    - servo to set or get position for. Default is 0.
+    S<position> - set position in degrees, 0 - 180.
+                  If omitted the current position is reported: 
+                  [Servo <index> position: <position> degrees]
+
+```
+
+Configuration:
+
+Add/uncomment `#define PWM_SERVO_ENABLE 1` in _my_machine.h_.
+
+Dependencies:
+
+The selected driver/board must provide at least one free auxiliary analog output port that is PWM capable.
+
+Credits:
+
+Based on [code](https://github.com/wakass/grlbhal_servo) by @wakass.
+
+### BLTouch probe
+
+*** Experimental ***
+
+Adds support for Marlin style [M401](https://marlinfw.org/docs/gcode/M401.html) and [M402](https://marlinfw.org/docs/gcode/M402.html) commands.
+
+```
+M401 [H] [R<0|1>] [S<0|1>] - deploy probe
+
+    H      - report high speed mode.
+    R<0|1> - currently ignored.
+    S<0|1> - S0: disable high speed mode, S1: enable high speed mode.
+```
+
+```
+M402 [R<0|1>] - stow probe
+
+    R<0|1> - currently ignored.
+```
+
+Configuration:
+
+Add/uncomment `#define PWM_SERVO_ENABLE 1` and `#define BLTOUCH_ENABLE 1` in _my_machine.h_.
+
+Dependencies:
+
+The selected driver/board must provide at least one free auxiliary analog output port that is PWM capable.
+
+Credits:
+
+Based on [code](https://github.com/wakass/grlbhal_servo) by @wakass.
+
+---
+2025-05-14
---
+++ main/plugins/eventout.c
@@ -0,0 +1,404 @@
+/*
+  eventout.c - plugin for binding some events to aux output pins
+
+  Part of grblHAL misc. plugins
+
+  Copyright (c) 2024-2025 Terje Io
+
+  grblHAL is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  grblHAL is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with grblHAL. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "driver.h"
+
+#if EVENTOUT_ENABLE == 1
+
+#include <stdio.h>
+#include <string.h>
+
+#include "grbl/nvs_buffer.h"
+#include "grbl/protocol.h"
+#include "grbl/task.h"
+
+#ifndef N_EVENTS
+#define N_EVENTS 4
+#endif
+
+// Sanity check
+#if N_EVENTS > 10
+#undef N_EVENTS
+#define N_EVENTS 10
+#endif
+
+#define EVENT_OPTS { .subgroups = Off, .increment = 1 }
+#define EVENT_OPTS_REBOOT { .subgroups = Off, .increment = 1, .reboot_required = On }
+#define EVENT_TRIGGERS "None,Spindle enable (M3/M4),Laser enable (M3/M4),Mist enable (M7),Flood enable (M8),Feed hold,Alarm"
+
+typedef enum {
+    Event_Ignore = 0,
+    Event_Spindle,
+    Event_Laser,
+    Event_Mist,
+    Event_Flood,
+    Event_FeedHold,
+    Event_Alarm
+} event_trigger_t;
+
+typedef struct {
+    uint8_t port;
+    event_trigger_t trigger;
+} event_setting_t;
+
+typedef struct {
+    event_setting_t event[N_EVENTS];
+} event_settings_t;
+
+static uint8_t n_ports, n_events;
+static uint8_t port[N_EVENTS];
+static char max_port[4];
+static nvs_address_t nvs_address;
+static event_settings_t plugin_settings;
+static on_report_options_ptr on_report_options;
+static driver_reset_ptr driver_reset;
+
+static coolant_set_state_ptr coolant_set_state_ = NULL;
+static on_spindle_programmed_ptr on_spindle_programmed;
+static on_state_change_ptr on_state_change;
+static bool on_spindle_programmed_attached = false;
+static bool on_state_change_attached = false;
+
+static void onReset (void)
+{
+    uint_fast16_t idx = n_events;
+
+    do {
+        if(port[--idx] != 0xFF && plugin_settings.event[idx].trigger && plugin_settings.event[idx].trigger != Event_Alarm)
+            ioport_digital_out(port[idx], 0);
+    } while(idx);
+
+    driver_reset();
+}
+
+static void onSpindleProgrammed (spindle_ptrs_t *spindle, spindle_state_t state, float rpm, spindle_rpm_mode_t mode)
+{
+    uint_fast16_t idx = n_events;
+
+    if(on_spindle_programmed)
+        on_spindle_programmed(spindle, state, rpm, mode);
+
+    do {
+        if(port[--idx] != 0xFF && plugin_settings.event[idx].trigger == (spindle->cap.laser ? Event_Laser : Event_Spindle))
+            ioport_digital_out(port[idx], state.on);
+    } while(idx);
+}
+
+static void onCoolantSetState (coolant_state_t state)
+{
+    uint_fast16_t idx = n_events;
+
+    coolant_set_state_(state);
+
+    do {
+        if(port[--idx] != 0xFF)
+          switch(plugin_settings.event[idx].trigger) {
+
+            case Event_Mist:
+                ioport_digital_out(port[idx], state.mist);
+                break;
+
+            case Event_Flood:
+                ioport_digital_out(port[idx], state.flood);
+                break;
+
+            default:
+                break;
+        }
+    } while(idx);
+}
+
+static void onStateChanged (sys_state_t state)
+{
+    static sys_state_t last_state = STATE_IDLE;
+
+    if(state != last_state) {
+
+        uint_fast16_t idx = n_events;
+
+        last_state = state;
+
+        do {
+            if(port[--idx] != 0xFF)
+              switch(plugin_settings.event[idx].trigger) {
+
+                case Event_FeedHold:
+                    ioport_digital_out(port[idx], state == STATE_HOLD);
+                    break;
+
+                case Event_Alarm:
+                    ioport_digital_out(port[idx], state == STATE_ALARM);
+                    break;
+
+                default: break;
+            }
+        } while(idx);
+    }
+
+    if(on_state_change)
+        on_state_change(state);
+}
+
+static void register_handlers (void)
+{
+    static char descr[N_EVENTS][25] = {0};
+
+    uint_fast16_t idx = n_events;
+
+    do {
+        if(port[--idx] != 0xFF)
+          switch(plugin_settings.event[idx].trigger) {
+            case Event_Laser:
+            case Event_Spindle:
+                sprintf(descr[idx], "P%d <- %s", port[idx], plugin_settings.event[idx].trigger == Event_Spindle ? "Spindle enable" : "Laser enable");
+                if(!on_spindle_programmed_attached) {
+                    on_spindle_programmed_attached = true;
+                    on_spindle_programmed = grbl.on_spindle_programmed;
+                    grbl.on_spindle_programmed = onSpindleProgrammed;
+                }
+                break;
+
+            case Event_Mist:
+            case Event_Flood:
+                sprintf(descr[idx], "P%d <- %s", port[idx], plugin_settings.event[idx].trigger == Event_Mist ? "Mist enable" : "Flood enable");
+                if(coolant_set_state_ == NULL) {
+                    coolant_set_state_ = hal.coolant.set_state;
+                    hal.coolant.set_state = onCoolantSetState;
+                }
+                break;
+
+            case Event_Alarm:
+            case Event_FeedHold:
+                sprintf(descr[idx], "P%d <- %s", port[idx], plugin_settings.event[idx].trigger == Event_Alarm ? "Alarm": "Feed hold");
+                if(!on_state_change_attached) {
+                    on_state_change_attached = true;
+                    on_state_change = grbl.on_state_change;
+                    grbl.on_state_change = onStateChanged;
+                }
+                break;
+
+            default:
+                sprintf(descr[idx], "P%d", port[idx]);
+                break;
+        }
+
+        ioport_set_description(Port_Digital, Port_Output, port[idx], descr[idx]);
+
+    } while(idx);
+}
+
+static setting_id_t normalize_id (setting_id_t setting, uint_fast16_t *idx)
+{
+    *idx = setting % 10;
+
+    return (setting_id_t)(setting - *idx);
+}
+
+static status_code_t set_int (setting_id_t setting, uint_fast16_t value)
+{
+    uint_fast16_t idx;
+    status_code_t status = Status_OK;
+
+    setting = normalize_id(setting, &idx);
+
+    plugin_settings.event[idx].trigger = (event_trigger_t)value;
+
+    return status;
+}
+
+static uint_fast16_t get_int (setting_id_t setting)
+{
+    uint_fast16_t idx;
+
+    setting = normalize_id(setting, &idx);
+
+    return plugin_settings.event[idx].trigger;
+}
+
+static status_code_t set_port (setting_id_t setting, float value)
+{
+    uint_fast16_t idx;
+    status_code_t status = Status_OK;
+
+    if(!isintf(value))
+        return Status_BadNumberFormat;
+
+    setting = normalize_id(setting, &idx);
+
+    plugin_settings.event[idx].port = value < 0.0f ? 0xFF : (uint8_t)value;
+
+    return status;
+}
+
+static float get_port (setting_id_t setting)
+{
+    float value;
+    uint_fast16_t idx;
+
+    setting = normalize_id(setting, &idx);
+
+    value = plugin_settings.event[idx].port >= n_ports ? -1.0f : (float)plugin_settings.event[idx].port;
+
+    return value;
+}
+
+static bool is_setting_available (const setting_detail_t *setting, uint_fast16_t offset)
+{
+    return offset < n_ports;
+}
+
+static const setting_detail_t event_settings[] = {
+    { Setting_ActionBase, Group_AuxPorts, "Event ? trigger", NULL, Format_RadioButtons, EVENT_TRIGGERS, NULL, NULL, Setting_NonCoreFn, set_int, get_int, is_setting_available, EVENT_OPTS },
+    { Setting_ActionPortBase, Group_AuxPorts, "Event ? port", NULL, Format_Decimal, "-#0", "-1", max_port, Setting_NonCoreFn, set_port, get_port, is_setting_available, EVENT_OPTS_REBOOT }
+};
+
+#ifndef NO_SETTINGS_DESCRIPTIONS
+
+static const setting_descr_t event_settings_descr[] = {
+    { Setting_ActionBase, "Event triggering output port change.\\n\\n"
+                          "NOTE: the port can still be controlled by M62-M65 commands even when bound to an event."},
+    { Setting_ActionPortBase, "Aux output port number to bind to the associated event trigger. Set to -1 to disable." }
+};
+
+#endif
+
+static void event_settings_save (void)
+{
+    hal.nvs.memcpy_to_nvs(nvs_address, (uint8_t *)&plugin_settings, sizeof(event_settings_t), true);
+}
+
+static void event_settings_restore (void)
+{
+    uint_fast8_t idx;
+
+    if(n_ports == 0 && (n_ports = ioports_unclaimed(Port_Digital, Port_Output)))
+        n_events = min(n_ports, N_EVENTS);
+
+    memset(&plugin_settings, 0xFF, sizeof(event_settings_t));
+
+    for(idx = 0; idx < n_events; idx++) {
+        switch(idx) {
+
+#ifdef EVENTOUT_1_ACTION
+            case 0:
+                plugin_settings.event[idx].trigger = (event_trigger_t)EVENTOUT_1_ACTION;
+                break;
+#endif
+#ifdef EVENTOUT_2_ACTION
+            case 1:
+                plugin_settings.event[idx].trigger = (event_trigger_t)EVENTOUT_2_ACTION;
+                break;
+#endif
+#ifdef EVENTOUT_3_ACTION
+            case 2:
+                plugin_settings.event[idx].trigger = (event_trigger_t)EVENTOUT_3_ACTION;
+                break;
+#endif
+#ifdef EVENTOUT_4_ACTION
+            case 3:
+                plugin_settings.event[idx].trigger = (event_trigger_t)EVENTOUT_4_ACTION;
+                break;
+#endif
+
+            default:
+                plugin_settings.event[idx].trigger = Event_Ignore;
+                break;
+        }
+        plugin_settings.event[idx].port = n_ports < (idx + 1) ? 0xFF : idx;
+    }
+
+    hal.nvs.memcpy_to_nvs(nvs_address, (uint8_t *)&plugin_settings, sizeof(event_settings_t), true);
+}
+
+static void event_settings_load (void)
+{
+    if(hal.nvs.memcpy_from_nvs((uint8_t *)&plugin_settings, nvs_address, sizeof(event_settings_t), true) != NVS_TransferResult_OK)
+        event_settings_restore();
+}
+
+static bool event_settings_iterator (const setting_detail_t *setting, setting_output_ptr callback, void *data)
+{
+    uint_fast16_t idx;
+
+    for(idx = 0; idx < n_events; idx++)
+        callback(setting, idx, data);
+
+    return true;
+}
+
+static void onReportOptions (bool newopt)
+{
+    on_report_options(newopt);
+
+    if(!newopt)
+        report_plugin("Events plugin", "0.08");
+}
+
+static void event_out_cfg (void *data)
+{
+    if((n_ports = ioports_unclaimed(Port_Digital, Port_Output))) {
+
+        n_events = min(n_ports, N_EVENTS);
+        strcpy(max_port, uitoa(n_ports - 1));
+
+        uint_fast16_t idx;
+        for(idx = 0; idx < n_events; idx++) {
+            if(plugin_settings.event[idx].port == 0xFF)
+                port[idx] = 0xFF;
+            else
+                port[idx] = min(plugin_settings.event[idx].port, n_ports - 1);
+        }
+
+        register_handlers();
+    }
+}
+
+void event_out_init (void)
+{
+    static setting_details_t setting_details = {
+        .settings = event_settings,
+        .n_settings = sizeof(event_settings) / sizeof(setting_detail_t),
+    #ifndef NO_SETTINGS_DESCRIPTIONS
+        .descriptions = event_settings_descr,
+        .n_descriptions = sizeof(event_settings_descr) / sizeof(setting_descr_t),
+    #endif
+        .save = event_settings_save,
+        .load = event_settings_load,
+        .restore = event_settings_restore,
+        .iterator = event_settings_iterator
+    };
+
+    if((nvs_address = nvs_alloc(sizeof(event_settings_t)))) {
+
+        settings_register(&setting_details);
+
+        on_report_options = grbl.on_report_options;
+        grbl.on_report_options = onReportOptions;
+
+        driver_reset = hal.driver_reset;
+        hal.driver_reset = onReset;
+
+        task_run_on_startup(event_out_cfg, NULL);
+    } else
+        task_run_on_startup(report_warning, "Events plugin failed to initialize!");
+}
+
+#endif // EVENTOUT_ENABLE
